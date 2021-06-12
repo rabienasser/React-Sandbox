024ccbe8bf12c28c15a5d69877b59ee53da01c93
Rabie <robienasser13@gmail.com>
2021-06-12 12:05:03 -0400

    monsters-grid-layout

--- Monsters Rolodex/monsters-rolodex/src/App.js
+++ Monsters Rolodex/monsters-rolodex/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -7,31 +7,25 @@ class App extends Component {
       super();
 
       this.state = {
-         monsters: [
-            {
-               name: "Frankenstein",
-               id: 1,
-            },
-            {
-               name: "Dracula",
-               id: 2,
-            },
-            {
-               name: "Zombie",
-               id: 3,
-            },
-         ],
+         monsters: [],
       };
+   }
+
+   // Fetching data from json.placeholder
+   componentDidMount() {
+      fetch("https://jsonplaceholder.typicode.com/users")
+         .then((res) => res.json())
+         .then((users) => this.setState({ monsters: users }));
    }
 
    render() {
       return (
          <div className="App">
-            {this.state.monsters.map((monster) => (
-               <h1>
-                  {monster.id} {monster.name}
-               </h1>
-            ))}
+            <CardList name="Rabie">
+               {this.state.monsters.map((monster) => (
+                  <h1 key={monster.id}> {monster.name}</h1>
+               ))}
+            </CardList>
          </div>
       );
    }
