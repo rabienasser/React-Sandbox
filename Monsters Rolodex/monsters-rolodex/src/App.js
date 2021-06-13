7ef3f16f92e2c73fa23ef82845f68a59be1488c6
Rabie <robienasser13@gmail.com>
2021-06-12 22:30:09 -0400

    monsters rolodex complete

--- Monsters Rolodex/monsters-rolodex/src/App.js
+++ Monsters Rolodex/monsters-rolodex/src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
 
       return (
          <div className="App">
+            <h1>Monsters Rolodex</h1>
             <SearchBox
                placeholder="Search Monsters"
                handleChange={this.handleChange}
