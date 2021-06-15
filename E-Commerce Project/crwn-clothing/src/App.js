aa3048fff7adc465fd6da367a70489b918fc3507
Rabie <robienasser13@gmail.com>
2021-06-14 22:00:31 -0400

    before checkout to prior commit

--- E-Commerce Project/crwn-clothing/src/App.js
+++ E-Commerce Project/crwn-clothing/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage-component";
 
