14be3e998d647c9fc68cf4270850ac1b521d0d5f
Rabie <robienasser13@gmail.com>
2021-06-16 15:00:42 -0400

    created input component with styling

--- E-Commerce Project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ E-Commerce Project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './sign-in.style.scss'
 
+import FormInput from '../form-input/form-input.component'
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props)
@@ -33,10 +35,21 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type="email" name="email" value={email} onChange={this.handleChange} required/>
-                    <label>Email</label>
-                    <input type="password" name="password" value={password} onChange={this.handleChange} required/>
-                    <label>Password</label>
+                    <FormInput 
+                    type="email" 
+                    name="email" 
+                    value={email} 
+                    handleChange={this.handleChange} 
+                    label="email"
+                    required/>
+
+                    <FormInput 
+                    type="password" 
+                    name="password" 
+                    value={password} 
+                    handleChange={this.handleChange} 
+                    label="password"
+                    required/>
 
                     <input type="submit" value="Submit" />
                 </form>
