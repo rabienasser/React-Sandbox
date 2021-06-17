80e7ad2eb0fa1dbf31ee7e2472a3f58d2046da50
Rabie <robienasser13@gmail.com>
2021-06-17 18:04:14 -0400

    button styling

--- E-Commerce Project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ E-Commerce Project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in.style.scss'
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
                     label="password"
                     required/>
 
-                    <input type="submit" value="Submit" />
+                    <CustomButton type="submit" value="Submit">Sign In</CustomButton>
                 </form>
             </div>
         )
