c27ff08578d8dc2d86b2f39c63d98f0304157400
Rabie <robienasser13@gmail.com>
2021-07-16 20:40:05 -0400

    created our-work page

--- portfolio/src/pages/OurWork.jsx
+++ portfolio/src/pages/OurWork.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+// Styled Components
+import styled from 'styled-components'
+
+// Images
+import athlete from '../img/athlete-small.png'
+import racer from '../img/theracer-small.png'
+import goodtimes from '../img/goodtimes-small.png'
 
 function OurWork() {
     return (
-        <div>
-            <h1>Our Work</h1>
-        </div>
+        <Work>
+            <Movie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete" />
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={racer} alt="racer" />
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="good times" />
+                </Link>
+            </Movie>
+        </Work>
     )
 }
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem 0rem 10rem;
+    color: #1b1b1b;
+`
+
+const Movie = styled.div`
+    padding-bottom: 10rem;
+
+    .line {
+        height: 0.5rem;
+        background: #23d997;
+        margin: 1rem 0 3rem 0;
+    }
+
+    img {
+        height: 70vh;
+        width: 100%;
+        object-fit: cover;
+    }
+
+
+`
 
 export default OurWork
